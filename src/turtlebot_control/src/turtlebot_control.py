7d15fb429a8903059b3ee3f826ba53e403d9606e
nflu <nflugovoy@berkeley.edu>
2019-12-10 12:50:21 -0800

    integrated control with planning, added PID control and arctan on error

--- src/turtlebot_control/src/turtlebot_control.py
+++ src/turtlebot_control/src/turtlebot_control.py
@@ -14,12 +14,13 @@ from collections import deque
 from geometry_msgs.msg import Twist, PointStamped
 import tf
 import ros_numpy
+import argparse
 
 
 # Define the method which contains the main functionality of the node.
 class Controller:
 
-    def __init__(self, turtlebot_frame, sub_topic, queue_size=10, max_deque_size=5):
+    def __init__(self, turtlebot_frame, sub_topic, use_arctan=False, queue_size=10, max_deque_size=5):
         """
     Controls a turtlebot whose position is denoted by turtlebot_frame,
     to go to a position denoted by target_frame
@@ -41,8 +42,22 @@ class Controller:
         # a 10Hz publishing rate
         self.r = rospy.Rate(10)  # 10hz
 
-        self.k1 = 0.3
-        self.k2 = -1.0
+        # control law
+        self.use_arctan = use_arctan
+        self.arctan_inner_gain = 8.0
+        self.arctan_outer_gain = 0.55
+
+        self.k_p = np.array([0.3, -1.0])
+
+        self.k_i = np.array([0, 0])
+
+        self.k_d = np.array([0, 0])
+
+        self.last_error = None
+        self.last_time = None
+
+        self.integral_error = 0.0
+        self.anti_windup = 1.0
 
         self.turtlebot_frame = turtlebot_frame
 
@@ -54,6 +69,9 @@ class Controller:
         if self.most_recent_goal is not None:
             try:
                 goal_frame = self.most_recent_goal.header.frame_id
+                time = np.float128(self.most_recent_goal.header.stamp.secs +
+                                   1e-9 * self.most_recent_goal.header.stamp.nsecs)
+
                 point = self.most_recent_goal.point
                 trans = self.tfBuffer.lookup_transform(self.turtlebot_frame, goal_frame, rospy.Time())
                 rot = ros_numpy.numpify(trans.transform.rotation)
@@ -62,9 +80,11 @@ class Controller:
                 # Generate a control command to send to the robot
                 msg = Twist()
                 point = np.dot(rot, ros_numpy.numpify(point)) + ros_numpy.numpify(trans.transform.translation)
-                print(point)
-                msg.linear.x = self.k1 *  point[1]
-                msg.angular.z = self.k2 *  point[0]
+
+                if self.use_arctan:
+
+                    msg.linear.x, msg.angular.z = self.control_law(np.array([point[1], point[0]]),
+                                                                   time)
 
                 self.pub.publish(msg)
                 # Use our rate object to sleep until it is time to publish again
@@ -73,6 +93,29 @@ class Controller:
                     tf2_ros.ExtrapolationException) as e:
                 print(e)
 
+    def control_law(self, error, time):
+
+        self.integral_error = self.anti_windup * self.integral_error + error
+
+        p = self.k_p * error
+        i = self.k_i * self.integral_error
+
+        if self.last_error:
+            delta_t = time - self.last_time
+            d = self.k_d * (error - self.last_error) / delta_t
+        else:
+            d = np.zeros(2)
+
+        if self.use_arctan:
+            p[0] = self.arctan_outer_gain * np.arctan(self.arctan_inner_gain * error[0])
+
+        # update errors
+        self.last_error = error
+        self.last_time = time
+
+        return p + i + d
+
+
 
 # This is Python's sytax for a main() method, which is run by default
 # when exectued in the shell
@@ -80,15 +123,25 @@ if __name__ == '__main__':
     # Check if the node has received a signal to shut down
     # If not, run the talker method
 
+    # set up command line arguments
+    parser = argparse.ArgumentParser(description="Control Module")
+    parser.add_argument('--goal_topic', type=str, help='defaults to /intersection_point')
+    parser.add_argument('--use_arctan', type=int, help='defaults to False')
+
+    # parse arguments
+    args = parser.parse_args()
+
     # Run this program as a new node in the ROS computation graph
     # called /turtlebot_controller.
     rospy.init_node('turtlebot_controller', anonymous=True)
 
     turtlebot_frame = 'base_link'
-    sub_topic = '/state_estimate'
+    sub_topic = args.goal_topic if args.goal_topic else '/intersection_point'
+    use_arctan = args.use_arctan if args.use_arctan else False
 
     try:
-        controller = Controller(turtlebot_frame=turtlebot_frame, sub_topic=sub_topic)
+        controller = Controller(turtlebot_frame=turtlebot_frame, sub_topic=sub_topic,
+                                use_arctan=use_arctan)
         while True:
             controller.publish_once_from_queue()
     except rospy.ROSInterruptException:
