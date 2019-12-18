fb017f1bb7245421748525cad0fe64585daeb513
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2019-12-18 14:47:07 -0800

    Got heading of robot and made the controller run at 30 Hz

--- src/turtlebot_control/src/spline_control.py
+++ src/turtlebot_control/src/spline_control.py
@@ -11,6 +11,7 @@ from collections import deque
 import tf
 import ros_numpy
 import numpy as np
+import argparse
 from prediction.msg import point_vel
 
 
@@ -20,21 +21,19 @@ MAX_OMEGA = 1.9
 
 class Controller:
     
-    def __init__(self, turtlebot_frame, prediction_topic, time_horizon=1.0, points_to_check=100, queue_size=10, max_deque_size=5):
+    def __init__(self, turtlebot_frame, world_frame, sub_topic, turtlebot_color, \
+        time_horizon=1.0, points_to_check=100, queue_size=10, max_deque_size=5):
         """
-    Controls a turtlebot whose position is denoted by turtlebot_frame,
-    to go to a position denoted by target_frame
-    Inputs:
-    - turtlebot_frame: the tf frame of the AR tag on your turtlebot
-    - target_frame: the tf frame of the target AR tag
-    """
+        Controls a turtlebot whose position is denoted by turtlebot_frame,
+        to go to a position denoted by target_frame
+        Inputs:
+        - turtlebot_frame: the tf frame of the AR tag on your turtlebot
+        - target_frame: the tf frame of the target AR tag
+        """
 
         # Create a publisher and a tf buffer, which is primed with a tf listener
-        self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=queue_size)
-        self.tfBuffer = tf2_ros.Buffer()
-        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
-
-        self.prediction_topic_sub = rospy.Subscriber(prediction_topic,
+        self.pub = rospy.Publisher('/' + turtlebot_color + '/mobile_base/commands/velocity', Twist, queue_size=queue_size)
+        self.prediction_topic_sub = rospy.Subscriber(sub_topic,
                                                 point_vel,
                                                 self.callback)
 
@@ -44,45 +43,64 @@ class Controller:
         self.points_to_check = points_to_check
 
         # Create a timer object that will sleep long enough to result in
-        # a 10Hz publishing rate
-        self.r = rospy.Rate(10)  # 10hz
+        # a 30Hz publishing rate
+        self.r = rospy.Rate(30) 
 
         self.turtlebot_frame = turtlebot_frame
+        self.world_frame = world_frame
     
     def callback(self, point):
         self.messages.appendleft(point)
     
     def publish_once_from_queue(self):
-        point_vel = self.messages.pop()
-        curr_point = point_vel.point
-        velocity = point_vel.linear
-        #time_of_sensor_measurement = curr_point.header.stamp
-        predictor_planner = Predictor_Planner(velocity.x, curr_point.x, velocity.y, curr_point.y)
-        robot_state = [curr_point.x, curr_point.y]
-        plan = predictor_planner.plan_to_intercept(robot_state, self.time_horizon, self.points_to_check)
-        #time_of_plan = rospy.Time.now()
-        time_achieved = plan.planning_time()
+        if len(self.messages):
+            self.most_recent_meas = self.messages.pop()
+            curr_point = self.most_recent_meas.point
+            velocity = self.most_recent_meas.linear
+            predictor_planner = Predictor_Planner(velocity.x, curr_point.x, velocity.y, curr_point.y, self.turtlebot_frame, self.world_frame)
+            robot_state = [curr_point.x, curr_point.y]
+            self.plan = predictor_planner.plan_to_intercept(robot_state, self.time_horizon, self.points_to_check)
+            time_achieved = self.plan.planning_time()  # Plan is normalized to happen in only 1 sec
 
-        #time_since_plan = rospy.Time.now() - time_of_plan
-        #converted_time = time_since_plan/time_achieved 	# Plan is normalized to happen in only 1 sec
-        #control_speed = plan.speed(converted_time)/time_achieved
-        #control_omega = plan.omega(converted_time)/time_achieved
-        control_x_vel = plan.x_dot(0)/time_achieved
-        control_y_vel = plan.y_dot(0)/time_achieved
-        control_omega = plan.omega(0)/time_achieved
+            control_x_vel = self.plan.x_dot(0)/time_achieved
+            control_y_vel = self.plan.y_dot(0)/time_achieved
+            control_omega = self.plan.omega(0)/time_achieved
 
-        # Generate a control command to send to the robot
-        msg = Twist()
-        msg.linear.x = control_x_vel
-        msg.linear.y = control_y_vel
-        msg.linear.z = 0
-        msg.angular.x = 0
-        msg.angular.y = 0
-        msg.angular.z = control_omega
+            # Generate a control command to send to the robot
+            msg = Twist()
+            msg.linear.x = control_x_vel
+            msg.linear.y = control_y_vel
+            msg.linear.z = 0
+            msg.angular.x = 0
+            msg.angular.y = 0
+            msg.angular.z = control_omega
 
-        self.pub.publish(msg)
-        # Use our rate object to sleep until it is time to publish again
-        self.r.sleep()
+            self.pub.publish(msg)
+            self.time = rospy.get_time()
+            # Use our rate object to sleep until it is time to publish again
+            self.r.sleep()
+
+        else:
+            time_achieved = self.plan.planning_time()   # Plan is normalized to happen in only 1 sec
+            converted_time = (self.time - rospy.get_time())/time_achieved   
+            control_x_vel = self.plan.x_dot(converted_time)/time_achieved
+            control_y_vel = self.plan.y_dot(converted_time)/time_achieved
+            control_omega = self.plan.omega(converted_time)/time_achieved
+
+            # Generate a control command to send to the robot
+            msg = Twist()
+            msg.linear.x = control_x_vel
+            msg.linear.y = control_y_vel
+            msg.linear.z = 0
+            msg.angular.x = 0
+            msg.angular.y = 0
+            msg.angular.z = control_omega
+
+            self.pub.publish(msg)
+            self.time = rospy.get_time()
+            # Use our rate object to sleep until it is time to publish again
+            self.r.sleep()
+
 
 
 class Plan:
@@ -150,11 +168,15 @@ class Plan:
 
 
 class Predictor_Planner:
-    def __init__(self, m1, b1, m2, b2):
+    def __init__(self, m1, b1, m2, b2, turtlebot_frame, world_frame):
         self.m1 = m1
         self.b1 = b1
         self.m2 = m2
         self.b2 = b2
+        self.turtlebot_frame = turtlebot_frame
+        self.world_frame = world_frame
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
 
     def future_location(self, t):
         at_t_x = self.m1 * t + self.b1
@@ -206,8 +228,13 @@ class Predictor_Planner:
         for t in ts:
             # Where is the ball? That's the goal for the planner
             at_t_x, at_t_y = self.future_location(t) 
-            # TODO: Use TF transform to get current robot heading
-            robot_theta = np.pi   ######################## THIS IS A PLACEHOLDER #######################
+
+            trans = self.tfBuffer.lookup_transform(self.turtlebot_frame, self.world_frame, rospy.Time())
+            translation = ros_numpy.numpify(trans.transform.translation)
+            robot_theta = np.arctan2(translation[1]/translation[0])
+            #rot = ros_numpy.numpify(trans.transform.rotation)
+            #rot = np.array(tf.transformations.quaternion_matrix(rot)[:3, :3])
+            
             final_state = [at_t_x, at_t_y, robot_theta]
             initial_speed = 0.2
             final_speed = 0.2
@@ -229,16 +256,32 @@ class Predictor_Planner:
 # This is Python's sytax for a main() method, which is run by default
 # when exectued in the shell
 if __name__ == '__main__':
+    # set up command line arguments
+    parser = argparse.ArgumentParser(description="Control Module")
+    parser.add_argument('color', type=str, help='color of turtlebot')
+    parser.add_argument('turtle_frame', type=str, help='frame of turtlebot')
+    parser.add_argument('world_frame', type=str, help='frame of AR tag on floor')
+    parser.add_argument('--goal_topic', type=str, help='defaults to /intersection_point')
+
+    # parse arguments
+    args = parser.parse_args()
+
     # Run this program as a new node in the ROS computation graph
     # called /turtlebot_controller.
-    rospy.init_node('turtlebot_controller', anonymous=True)
+    rospy.init_node('spline_turtlebot_controller', anonymous=True)
 
-    turtlebot_frame = 'base_link'
-    prediction_topic = '/predicted_path'
-    try:
-        controller = Controller(turtlebot_frame=turtlebot_frame, prediction_topic=prediction_topic)
-        
+    turtlebot_frame = args.turtle_frame if args.turtle_frame else 'base_link'
+    sub_topic = args.goal_topic if args.goal_topic else '/predicted_point'
+    turtlebot_color = args.color 
+    world_frame = args.world_frame if args.world_frame else 'ar_marker_13'
+    
+    controller = Controller(turtlebot_frame=turtlebot_frame, world_frame=world_frame, sub_topic=sub_topic,
+                            turtlebot_color=turtlebot_color)
+    while True:
+        raw_input('press enter to run')
+        print('\n running!')
         while True:
             controller.publish_once_from_queue()
-    except rospy.ROSInterruptException:
-        pass
+
+    #turtlebot_frame = 'base_link'
+    #prediction_topic = '/predicted_path'
